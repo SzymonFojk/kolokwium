36c0c87f756bc7574ab5237120cd8e92803752b9
Szymon <szymon@pop-os.localdomain>
2021-01-29 16:52:31 +0100

    Commit nr1

---
+++ build/CMakeFiles/Student.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Student.dir/home/szymon/Pulpit/kolo/kolokwium/kol_1/zad3/src/Student.cpp.o"
+  "Student"
+  "Student.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Student.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib.dir/home/szymon/Pulpit/kolo/kolokwium/kol_1/zad3/src/libtest.cpp.o"
+  "liblib.a"
+  "liblib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
